868d38a99695727db6fcd135b5368e32d60be70e
Canmaya <kuebra.canmaya@outlook.com>
2022-09-20 18:56:00 +0200

    implemented get all members method

--- src/main/java/ch/zli/coworkingSpace/controller/MemberController.java
+++ src/main/java/ch/zli/coworkingSpace/controller/MemberController.java
@@ -1,5 +1,32 @@
 package ch.zli.coworkingSpace.controller;
 
-public class MemberController {
+import ch.zli.coworkingSpace.model.MemberEntity;
+import ch.zli.coworkingSpace.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members")
+    public ResponseEntity<List<MemberEntity>> getMembers() {
+        return ResponseEntity
+                .status(HttpStatus.OK) // HTTP 200
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(memberService.loadAll());
+    }
 }
+
