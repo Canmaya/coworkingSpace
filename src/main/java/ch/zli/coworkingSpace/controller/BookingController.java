88cbc7fb923a9b3d94071dce62d8aadee1d3e730
Canmaya <kuebra.canmaya@outlook.com>
2022-09-20 11:26:48 +0200

    implemented get all bookings

--- src/main/java/ch/zli/coworkingSpace/controller/BookingController.java
+++ src/main/java/ch/zli/coworkingSpace/controller/BookingController.java
@@ -1,8 +1,13 @@
 package ch.zli.coworkingSpace.controller;
 
+import ch.zli.coworkingSpace.model.BookingEntity;
 import ch.zli.coworkingSpace.service.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BookingController {
@@ -11,5 +16,12 @@ public class BookingController {
     @Autowired
     public BookingController(BookingService bookingService) {
         this.bookingService = bookingService;
+    }
+    @GetMapping("/bookings")
+    public ResponseEntity<Iterable<BookingEntity>> getBookingDates() {
+        return ResponseEntity
+                .status(HttpStatus.OK) // HTTP 200 = OK
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(bookingService.loadAll());
     }
 }
