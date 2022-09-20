19e8c61de24852beac8684ce5a066733bf1aa170
Canmaya <kuebra.canmaya@outlook.com>
2022-09-20 11:05:39 +0200

    created constructor

--- src/main/java/ch/zli/coworkingSpace/controller/BookingController.java
+++ src/main/java/ch/zli/coworkingSpace/controller/BookingController.java
@@ -1,2 +1,15 @@
-package ch.zli.coworkingSpace.controller;public class BookingController {
+package ch.zli.coworkingSpace.controller;
+
+import ch.zli.coworkingSpace.service.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BookingController {
+    private final BookingService bookingService;
+
+    @Autowired
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
 }
--- src/main/java/ch/zli/coworkingSpace/service/BookingService.java
+++ src/main/java/ch/zli/coworkingSpace/service/BookingService.java
@@ -1,5 +1,44 @@
 package ch.zli.coworkingSpace.service;
 
-public class BookingService {
+import ch.zli.coworkingSpace.model.BookingEntity;
+import ch.zli.coworkingSpace.repository.BookingRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
+@Service
+@Slf4j
+public class BookingService {
+    private final BookingRepository repository;
+
+    public BookingService(BookingRepository BookingRepository) {
+        this.repository = BookingRepository;
+    }
+
+    public List<BookingEntity> loadAll() {
+        log.info("Executing find all booking dates ...");
+        return (List<BookingEntity>) repository.findAll();
+    }
+
+    public Optional<BookingEntity> loadOne(Long gameId) {
+        log.info("Executing find booking date with id " + gameId + " ...");
+        return repository.findById(gameId);
+    }
+
+    public BookingEntity create(BookingEntity bookingDate) {
+        log.info("Executing create booking date with id " + bookingDate.getId() + " ...");
+        return repository.save(bookingDate);
+    }
+
+    public BookingEntity update(BookingEntity updateBookingDate) {
+        log.info("Executing update booking date with id " + updateBookingDate.getId() + " ...");
+
+        return repository.save(updateBookingDate);
+    }
+
+    public void delete(Long bookingDateId) {
+        log.info("Executing delete game with id " + bookingDateId + " ...");
+        repository.deleteById(bookingDateId);
+    }
 }
