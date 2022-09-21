3ba01446c09a0d5d3c52e8a9de50420c7daa2a4c
Canmaya <kuebra.canmaya@outlook.com>
2022-09-21 07:29:42 +0200

    implemented integration test template

---
+++ src/test/java/ch/zli/coworkingSpace/BookingControllerIntegrationTest.java
@@ -0,0 +1,51 @@
+package ch.zli.coworkingSpace;
+
+import aj.org.objectweb.asm.TypeReference;
+import ch.zli.coworkingSpace.model.BookingEntity;
+import ch.zli.coworkingSpace.security.JwtServiceHMAC;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BookingControllerIntegrationTest {
+    /*
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private JwtServiceHMAC jwtService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void allBookingsShouldBeReturnedFromService() throws Exception {
+        val accessToken = jwtService.createNewJWT(UUID.randomUUID().toString(), "9135f12e-1b66-4ee6-bbae-df37303cc154", "admin", List.of("ADMIN"));
+
+        val response = mockMvc.perform(get("/bookings").header("Authorization", "Bearer " + accessToken))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+
+        List<BookingEntity> bookings = objectMapper.readValue(response.getResponse().getContentAsString(), new TypeReference<>() {});
+
+        assertEquals(3, bookings.size());
+    }
+
+     */
+
+}
